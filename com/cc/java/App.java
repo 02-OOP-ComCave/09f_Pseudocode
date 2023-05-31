901fd612f4bf7473b3c8cde1721d7e207d569db0
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-05-31 15:03:33 +0200

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,75 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+     
+       /* 1. IF - ELSE */
+
+        // int a;
+        // int b;
+
+        // if (a>b) {
+        //     System.out.println("a > b ");
+        // } else {
+        //     System.out.println("a < b ");
+        // }
+
+        /* 2. SWITCH CASE */
+
+        // String op;
+
+        // switch (op) {
+        //     case "+":
+        //         System.out.println("Addition");
+        //         break;
+        //     case "-":
+        //         System.out.println("Subtraktion");
+        //         break;
+        //     case "*":
+        //         System.out.println("Multiplikation");
+        //         break;
+        //     case ":":
+        //         System.out.println("Division");
+        //         break;
+        //     default:
+        //         System.out.println("??");
+        //         break;
+        // }
+
+        /* 3. FOR Schleife */
+
+        // int[] arr= {1,2,3,4};
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
+        // }
+
+        /* 4. do - while Schleife */
+
+            // int a = 0;
+
+            // do {
+            //     a += 1;
+            // } while (a < 5);
+
+        
+    }
+
+    /* 5. Funktionen */
+    
+    public int summe(int a, int b) {
+        return a + b;
+    }
+
+
+
+
+    public static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+
+
+}
+
